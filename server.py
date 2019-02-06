6ef621432f1a99bdcf61fbfae5c4a08889ba9c31
Ryan <ryguyheaton@gmail.com>
2019-02-06 15:09:49 -0500

    started the server

---
+++ server.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask
+app = Flask(__name__)
+
+os.environ['FLASK_DEBUG'] = "1" # DO NOT USE IN PRODUCTION
+
+@app.route('/hello/')
+def hello():
+    return "Hello World!"
+
+@app.route('/user/<username>')
+def showUsername(username):
+    return username
+
+if __name__ == '__main__':
+    app.run()
