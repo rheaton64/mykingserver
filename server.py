0d00d037a085409cc8e6b1d5f69b7cf08b229a3a
Ryan <ryguyheaton@gmail.com>
2019-02-08 08:51:26 -0500

    Created method and route headers

--- server.py
+++ server.py
@@ -2,17 +2,63 @@ import os
 
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask import request
 app = Flask(__name__)
 
 os.environ['FLASK_DEBUG'] = "1" # DO NOT USE IN PRODUCTION
 
-@app.route('/hello/')
-def hello():
-    return "Hello World!"
+# returns a list of assignments that match the student token
+@app.route('/<token>/assignments/', method = ['GET'])
+def getAssignments(token):
+    return token
 
-@app.route('/user/<username>')
-def showUsername(username):
-    return username
+# returns a list of classes that match the student token
+@app.route('/<token>/classes/', methods = ['GET'])
+def getClasses(token):
+    return token
+
+# returns a schedule that matches the student token
+@app.route('/<token>/schedule/', methods = ['GET'])
+def getSchedule(token):
+    return token
+
+# returns all current active announcements
+@app.route('/announcements/', methods = ['GET'])
+def getAnnouncements():
+    return "Hello"
+
+# called when a valid token wants tp:
+# GET: See all their announcements that they have posted
+# POST: Update / Edit one of the announcements
+# PUT: Add a new announcement
+@app.route('/announcements/<token>/<value>', methods = ['GET', 'POST', 'PUT'])
+def announce(token, value):
+    if request.method == 'GET':
+       getAnnouncementsFromToken(token)
+    if request.method == 'POST':
+        updateAnnouncement(token, value)
+    else:
+        addAnnouncement(token, value)
+
+# Gets all announcements accessible from a token
+def getAnnouncementsFromToken(token):
+    return None
+
+# updates announement, given token for credentials and value for data 
+def updateAnnouncement(token, value):
+    return None
+
+# adds a new announcements, given token for credentials and value for data
+def addAnnouncement(token, value):
+    return None
+
+# return the letter dat based on the calendar day
+def getDay():
+    return None
+
+# reHashes a token back into a student ID
+def getIDFromToken(token):
+    return token
 
 if __name__ == '__main__':
     app.run()
